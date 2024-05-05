aee1fd7f102d8c298bea809cc2a198604bf47f9c
Caio Oliveira <caio.oliveira07@gmail.com>
2024-05-05 17:52:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculadora-Maior-Numero
+ Programa que calcula o maior número entre 3 que o usuário irá escolher
---
+++ script.js
@@ -0,0 +1,20 @@
+function encontrarMaior() {
+    // Entrada
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var num3 = parseFloat(document.getElementById("num3").value);
+
+    // Processamento
+    var maior;
+
+    if (num1 >= num2 && num1 >= num3) {
+        maior = num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        maior = num2;
+    } else {
+        maior = num3;
+    }
+
+    // Saída
+    document.getElementById("resultado").innerText = `O maior número é: ${maior}`;
+}
